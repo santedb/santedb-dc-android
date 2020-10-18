00744101e4da43c61749f882ad7a84b70ee1e7b7
Justin Fyfe <justin@fyfesoftware.ca>
2020-10-18 14:25:17 -0400

    Updated version refs

--- SanteDB.DisconnectedClient.Android/Properties/AssemblyInfo.cs
+++ SanteDB.DisconnectedClient.Android/Properties/AssemblyInfo.cs
@@ -37,7 +37,7 @@ using Android.App;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("2.0.42.*")]
+[assembly: AssemblyVersion ("2.0.43.*")]
 [assembly: AssemblyInformationalVersion("Langley")]
 
 // The following attributes are used to specify the signing key for the assembly,
--- SanteDB.DisconnectedClient.Android/Resources/Resource.designer.cs
+++ SanteDB.DisconnectedClient.Android/Resources/Resource.designer.cs
@@ -26,30 +26,6 @@ namespace SanteDBAndroid
 		
 		public static void UpdateIdValues()
 		{
-			global::SanteDB.DisconnectedClient.Android.Core.Resource.Id.contentFrame = global::SanteDBAndroid.Resource.Id.contentFrame;
-			global::SanteDB.DisconnectedClient.Android.Core.Resource.Layout.zxingscanneractivitylayout = global::SanteDBAndroid.Resource.Layout.zxingscanneractivitylayout;
-			global::SanteDB.DisconnectedClient.Android.Core.Resource.Layout.zxingscannerfragmentlayout = global::SanteDBAndroid.Resource.Layout.zxingscannerfragmentlayout;
-			global::SanteDB.DisconnectedClient.Android.Core.Resource.String.applet_confirm_close = global::SanteDBAndroid.Resource.String.applet_confirm_close;
-			global::SanteDB.DisconnectedClient.Android.Core.Resource.String.cancel = global::SanteDBAndroid.Resource.String.cancel;
-			global::SanteDB.DisconnectedClient.Android.Core.Resource.String.confirm = global::SanteDBAndroid.Resource.String.confirm;
-			global::SanteDB.DisconnectedClient.Android.Core.Resource.String.err_general = global::SanteDBAndroid.Resource.String.err_general;
-			global::SanteDB.DisconnectedClient.Android.Core.Resource.String.err_oauth2_invalid_client = global::SanteDBAndroid.Resource.String.err_oauth2_invalid_client;
-			global::SanteDB.DisconnectedClient.Android.Core.Resource.String.err_oauth2_invalid_grant = global::SanteDBAndroid.Resource.String.err_oauth2_invalid_grant;
-			global::SanteDB.DisconnectedClient.Android.Core.Resource.String.err_oauth2_invalid_request = global::SanteDBAndroid.Resource.String.err_oauth2_invalid_request;
-			global::SanteDB.DisconnectedClient.Android.Core.Resource.String.err_oauth2_invalid_scope = global::SanteDBAndroid.Resource.String.err_oauth2_invalid_scope;
-			global::SanteDB.DisconnectedClient.Android.Core.Resource.String.err_oauth2_unauthorized_client = global::SanteDBAndroid.Resource.String.err_oauth2_unauthorized_client;
-			global::SanteDB.DisconnectedClient.Android.Core.Resource.String.err_oauth2_unsupported_grant_type = global::SanteDBAndroid.Resource.String.err_oauth2_unsupported_grant_type;
-			global::SanteDB.DisconnectedClient.Android.Core.Resource.String.err_token_InvalidSignature = global::SanteDBAndroid.Resource.String.err_token_InvalidSignature;
-			global::SanteDB.DisconnectedClient.Android.Core.Resource.String.err_token_InvalidTokenType = global::SanteDBAndroid.Resource.String.err_token_InvalidTokenType;
-			global::SanteDB.DisconnectedClient.Android.Core.Resource.String.err_token_KeyNotFound = global::SanteDBAndroid.Resource.String.err_token_KeyNotFound;
-			global::SanteDB.DisconnectedClient.Android.Core.Resource.String.err_token_NotYetValid = global::SanteDBAndroid.Resource.String.err_token_NotYetValid;
-			global::SanteDB.DisconnectedClient.Android.Core.Resource.String.err_token_TokenExpired = global::SanteDBAndroid.Resource.String.err_token_TokenExpired;
-			global::SanteDB.DisconnectedClient.Android.Core.Resource.String.library_name = global::SanteDBAndroid.Resource.String.library_name;
-			global::SanteDB.DisconnectedClient.Android.Core.Resource.String.startup = global::SanteDBAndroid.Resource.String.startup;
-			global::SanteDB.DisconnectedClient.Android.Core.Resource.String.startup_configuration = global::SanteDBAndroid.Resource.String.startup_configuration;
-			global::SanteDB.DisconnectedClient.Android.Core.Resource.String.startup_data = global::SanteDBAndroid.Resource.String.startup_data;
-			global::SanteDB.DisconnectedClient.Android.Core.Resource.String.startup_setup = global::SanteDBAndroid.Resource.String.startup_setup;
-			global::SanteDB.DisconnectedClient.Android.Core.Resource.String.startup_update = global::SanteDBAndroid.Resource.String.startup_update;
 			global::SQLite.Net.Platform.XamarinAndroid.Resource.String.ApplicationName = global::SanteDBAndroid.Resource.String.ApplicationName;
 			global::SQLite.Net.Platform.XamarinAndroid.Resource.String.Hello = global::SanteDBAndroid.Resource.String.Hello;
 			global::ZXing.Mobile.Resource.Id.contentFrame = global::SanteDBAndroid.Resource.Id.contentFrame;
@@ -191,164 +167,125 @@ namespace SanteDBAndroid
 			// aapt resource value: 0x7F060003
 			public const int activity_setup = 2131099651;
 			
-			// aapt resource value: 0x7F060005
-			public const int applet_confirm_close = 2131099653;
-			
 			// aapt resource value: 0x7F060000
 			public const int ApplicationName = 2131099648;
 			
 			// aapt resource value: 0x7F060004
 			public const int app_name = 2131099652;
 			
+			// aapt resource value: 0x7F060005
+			public const int cancel = 2131099653;
+			
 			// aapt resource value: 0x7F060006
-			public const int cancel = 2131099654;
+			public const int confirm = 2131099654;
 			
 			// aapt resource value: 0x7F060007
-			public const int confirm = 2131099655;
+			public const int err_applet_not_found = 2131099655;
 			
 			// aapt resource value: 0x7F060008
-			public const int err_applet_not_found = 2131099656;
+			public const int err_exception = 2131099656;
 			
 			// aapt resource value: 0x7F060009
-			public const int err_exception = 2131099657;
+			public const int err_general = 2131099657;
 			
 			// aapt resource value: 0x7F06000A
-			public const int err_general = 2131099658;
+			public const int err_login = 2131099658;
 			
 			// aapt resource value: 0x7F06000B
-			public const int err_login = 2131099659;
+			public const int err_login_invalidusername = 2131099659;
 			
 			// aapt resource value: 0x7F06000C
-			public const int err_login_invalidusername = 2131099660;
+			public const int err_online_required = 2131099660;
 			
 			// aapt resource value: 0x7F06000D
-			public const int err_oauth2_invalid_client = 2131099661;
-			
-			// aapt resource value: 0x7F06000E
-			public const int err_oauth2_invalid_grant = 2131099662;
-			
-			// aapt resource value: 0x7F06000F
-			public const int err_oauth2_invalid_request = 2131099663;
-			
-			// aapt resource value: 0x7F060010
-			public const int err_oauth2_invalid_scope = 2131099664;
-			
-			// aapt resource value: 0x7F060011
-			public const int err_oauth2_unauthorized_client = 2131099665;
-			
-			// aapt resource value: 0x7F060012
-			public const int err_oauth2_unsupported_grant_type = 2131099666;
-			
-			// aapt resource value: 0x7F060013
-			public const int err_online_required = 2131099667;
-			
-			// aapt resource value: 0x7F060014
-			public const int err_startup = 2131099668;
-			
-			// aapt resource value: 0x7F060015
-			public const int err_token_InvalidSignature = 2131099669;
-			
-			// aapt resource value: 0x7F060016
-			public const int err_token_InvalidTokenType = 2131099670;
-			
-			// aapt resource value: 0x7F060017
-			public const int err_token_KeyNotFound = 2131099671;
-			
-			// aapt resource value: 0x7F060018
-			public const int err_token_NotYetValid = 2131099672;
-			
-			// aapt resource value: 0x7F060019
-			public const int err_token_TokenExpired = 2131099673;
+			public const int err_startup = 2131099661;
 			
 			// aapt resource value: 0x7F060001
 			public const int Hello = 2131099649;
 			
+			// aapt resource value: 0x7F06000E
+			public const int installing_applets = 2131099662;
+			
+			// aapt resource value: 0x7F06000F
+			public const int joinRealm = 2131099663;
+			
+			// aapt resource value: 0x7F060010
+			public const int leaveRealm = 2131099664;
+			
+			// aapt resource value: 0x7F060011
+			public const int loading = 2131099665;
+			
+			// aapt resource value: 0x7F060012
+			public const int login = 2131099666;
+			
+			// aapt resource value: 0x7F060013
+			public const int login_title = 2131099667;
+			
+			// aapt resource value: 0x7F060014
+			public const int needs_setup = 2131099668;
+			
+			// aapt resource value: 0x7F060015
+			public const int noRealm = 2131099669;
+			
+			// aapt resource value: 0x7F060016
+			public const int password = 2131099670;
+			
+			// aapt resource value: 0x7F060017
+			public const int setting_log = 2131099671;
+			
+			// aapt resource value: 0x7F060018
+			public const int setting_log_desc = 2131099672;
+			
+			// aapt resource value: 0x7F060019
+			public const int setting_offline_only = 2131099673;
+			
 			// aapt resource value: 0x7F06001A
-			public const int installing_applets = 2131099674;
+			public const int setting_offline_only_desc = 2131099674;
 			
 			// aapt resource value: 0x7F06001B
-			public const int joinRealm = 2131099675;
+			public const int setup_subTitle = 2131099675;
 			
 			// aapt resource value: 0x7F06001C
-			public const int leaveRealm = 2131099676;
+			public const int setup_subTitle_app = 2131099676;
 			
 			// aapt resource value: 0x7F06001D
-			public const int library_name = 2131099677;
+			public const int setup_subTitle_lnf = 2131099677;
 			
 			// aapt resource value: 0x7F06001E
-			public const int loading = 2131099678;
+			public const int setup_subTitle_sec = 2131099678;
 			
 			// aapt resource value: 0x7F06001F
-			public const int login = 2131099679;
+			public const int setup_title = 2131099679;
 			
 			// aapt resource value: 0x7F060020
-			public const int login_title = 2131099680;
+			public const int setup_title_app = 2131099680;
 			
 			// aapt resource value: 0x7F060021
-			public const int needs_setup = 2131099681;
+			public const int setup_title_lnf = 2131099681;
 			
 			// aapt resource value: 0x7F060022
-			public const int noRealm = 2131099682;
+			public const int setup_title_sec = 2131099682;
 			
 			// aapt resource value: 0x7F060023
-			public const int password = 2131099683;
+			public const int starting_services = 2131099683;
 			
 			// aapt resource value: 0x7F060024
-			public const int setting_log = 2131099684;
+			public const int startup = 2131099684;
 			
 			// aapt resource value: 0x7F060025
-			public const int setting_log_desc = 2131099685;
+			public const int startup_configuration = 2131099685;
 			
 			// aapt resource value: 0x7F060026
-			public const int setting_offline_only = 2131099686;
+			public const int startup_data = 2131099686;
 			
 			// aapt resource value: 0x7F060027
-			public const int setting_offline_only_desc = 2131099687;
+			public const int startup_setup = 2131099687;
 			
 			// aapt resource value: 0x7F060028
-			public const int setup_subTitle = 2131099688;
+			public const int startup_update = 2131099688;
 			
 			// aapt resource value: 0x7F060029
-			public const int setup_subTitle_app = 2131099689;
-			
-			// aapt resource value: 0x7F06002A
-			public const int setup_subTitle_lnf = 2131099690;
-			
-			// aapt resource value: 0x7F06002B
-			public const int setup_subTitle_sec = 2131099691;
-			
-			// aapt resource value: 0x7F06002C
-			public const int setup_title = 2131099692;
-			
-			// aapt resource value: 0x7F06002D
-			public const int setup_title_app = 2131099693;
-			
-			// aapt resource value: 0x7F06002E
-			public const int setup_title_lnf = 2131099694;
-			
-			// aapt resource value: 0x7F06002F
-			public const int setup_title_sec = 2131099695;
-			
-			// aapt resource value: 0x7F060030
-			public const int starting_services = 2131099696;
-			
-			// aapt resource value: 0x7F060031
-			public const int startup = 2131099697;
-			
-			// aapt resource value: 0x7F060032
-			public const int startup_configuration = 2131099698;
-			
-			// aapt resource value: 0x7F060033
-			public const int startup_data = 2131099699;
-			
-			// aapt resource value: 0x7F060034
-			public const int startup_setup = 2131099700;
-			
-			// aapt resource value: 0x7F060035
-			public const int startup_update = 2131099701;
-			
-			// aapt resource value: 0x7F060036
-			public const int user_name = 2131099702;
+			public const int user_name = 2131099689;
 			
 			static String()
 			{
