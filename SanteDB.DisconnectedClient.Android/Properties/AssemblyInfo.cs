eb50839c84a79381093f9209e062415ec92c7abe
Justin Fyfe <justin@fyfesoftware.ca>
2020-11-28 17:24:11 -0500

    Updated to new version for local user issue

--- SanteDB.DisconnectedClient.Android/Properties/AssemblyInfo.cs
+++ SanteDB.DisconnectedClient.Android/Properties/AssemblyInfo.cs
@@ -37,7 +37,7 @@ using Android.App;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("2.0.54.*")]
+[assembly: AssemblyVersion ("2.0.55.*")]
 [assembly: AssemblyInformationalVersion("Montreal")]
 
 // The following attributes are used to specify the signing key for the assembly,
